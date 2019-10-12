e41f19e0cc36ec7135b44abd1dd03454c1617099
rannny <1369296693@qq.com>
2019-10-12 21:39:59 +0800

    第10题正则表达式匹配动态规划自底向上正确运行 and 超时的自顶向下

--- regularmatch.cpp
+++ regularmatch.cpp
@@ -66,10 +66,11 @@ bool match(std::string s, std::string p) {
     else return false;
 }
 
+//回溯法
 bool isMatchRecursive(std::string s, std::string p) {
     if (p.empty())return s.empty();
     bool firstMatch = !s.empty() && (s[0] == p[0] || p[0] == '.');
-    if (p.size() >= 2 && p[1] == '*') {
+    if (p.size() >= 2 && p[1] == '*') {//保证存在两个字符且第二个字符等于*
         return (firstMatch && isMatchRecursive(s.substr(1), p)) || (isMatchRecursive(s, p.substr(2)));
     } else {
         return firstMatch && isMatchRecursive(s.substr(1), p.substr(1));
@@ -85,12 +86,14 @@ bool dp(int i,int j,std::string s,std::string p){
     if(matchVector->at(i).at(j)!= NULL)
         return matchVector->at(i).at(j);
     if(j==p.size())
-        matchVector->at(i).at(j)= i==s.size();
+        matchVector->at(i).at(j)= i==s.size();//0个字符到s.size()个字符总共是s.size()+1个字符
     else{
         bool firstMatch=i<s.size() && (p[j]==s[i]||p[j]=='.');
-        if(j+1<p.size()&&p[j+1]=='*'){
-            matchVector->at(i).at(j)=dp(i,j+2,s,p)||(firstMatch&&dp(i+1,j,s,p));
-        } else{
+
+        //保证第二位至少是p的最后一位且等于*
+        if(j+1<p.size() && p[j+1]=='*'){
+            matchVector->at(i).at(j)=(firstMatch && dp(i+1,j,s,p))||dp(i,j+2,s,p);
+        } else{//当前位是p的最后一位 或者 第二位不等于* 那么字符串各前进一位
             matchVector->at(i).at(j)=firstMatch&&dp(i+1,j+1,s,p);
         }
     }
@@ -98,10 +101,29 @@ bool dp(int i,int j,std::string s,std::string p){
 }
 
 bool isMatchDynamicUpToDown(std::string s, std::string p) {
-    matchVector->resize(s.size()+1);
+    std::vector<bool> vector(p.size()+1, false);
+    matchVector=std::make_shared<std::vector<std::vector<bool>>>(s.size()+1,vector);
     return dp(0,0,s,p);
 }
 
+bool isMatchDynamicDownToUp(std::string s, std::string p) {
+    std::vector<bool> vector(p.size()+1);
+    std::vector<std::vector<bool>> matchV(s.size()+1,vector);
+    matchV[s.size()][p.size()]= true;
+    for(int i=s.size();i>=0;i--){
+        //当j=p.size()的时候判断时只能根据字符串各前进一位判断
+        for(int j=p.size()-1;j>=0;j--){
+            bool firstMatch=i<s.size() && (p[j]==s[i]||p[j]=='.');
+            if(p[j+1]=='*')
+                matchV[i][j]=(firstMatch&&matchV[i+1][j])||matchV[i][j+2];
+            else//p[j+1]不等于* 或者 j+1是p的最后一位
+                matchV[i][j]=firstMatch&&matchV[i+1][j+1];
+        }
+    }
+    return matchV[0][0];
+}
+
 int main(){
-    std::cout<<isMatchDynamicUpToDown("ab",".*")<<std::endl;
+    std::cout<<isMatchDynamicUpToDown("aaaaaaaaaaaaab"
+                                      ,"a*a*a*a*a*a*a*a*a*a*c")<<std::endl;
 }
