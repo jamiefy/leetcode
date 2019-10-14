f4a28f1954d550673d2dd0a123009faa99fa65a8
rannny <1369296693@qq.com>
2019-10-14 20:21:27 +0800

    第38题报数 code不重要，思路最重要，一定要先确定思路再code

--- countandsay.cpp
+++ countandsay.cpp
@@ -35,6 +35,70 @@ std::string countAndSay(int n) {
     return countSequenceVector[n-1];
 }
 
+//上面方法的简化，把vector换成前一个string和当前string
+std::string countAndSaySimple(int n) {
+    //初始化第一个报数序列
+    std::string preCountSequence="1";
+
+    //n个报数序列循环n-1次
+    for(int i=1;i<n;i++){
+        //初始化第二个报数序列为空
+        std::string s;
+        //记录每组相同字符和字符数,字符改变则charCount从空开始
+        int charCount=0;
+        //对于该报数序列循环从头countSequenceindex=0开始,依次对每个字符进行处理,当索引=该报数序列的长度-1时循环停止
+        for(int countSequenceindex=0;countSequenceindex<preCountSequence.size();countSequenceindex++){
+
+            //在该报数序列长度范围内遍历相邻的相同字符,当字符发生改变从头开始计数,此时countcountSequenceindex指向发生改变的字符
+            if(countSequenceindex==0||preCountSequence[countSequenceindex-1]==preCountSequence[countSequenceindex])
+                charCount+=1;
+            else{
+                //在报数序列末尾依次添上每组相同字符数和字符
+                s+=std::to_string(charCount)+preCountSequence[countSequenceindex-1];
+                charCount=1;
+            }
+            //与前一位相等时最后一组的charCount和tmp未插入字符串;
+            //与前一位不相等时当前位的charCount和tmp未插入字符串
+            if(countSequenceindex==preCountSequence.size()-1)
+                s+=std::to_string(charCount)+preCountSequence[countSequenceindex];
+            }
+        preCountSequence=s;
+    }
+    //返回最后一个报数序列
+    return preCountSequence;
+}
+
+std::string countAndSayLine(std::string preCountSequence){
+    //初始化第二个报数序列为空
+    std::string s;
+    //记录每组相同字符和字符数,字符改变则charCount从空开始
+    int charCount=0;
+    //对于该报数序列循环从头countSequenceindex=0开始,依次对每个字符进行处理,当索引=该报数序列的长度-1时循环停止
+    for(int countSequenceindex=0;countSequenceindex<preCountSequence.size();countSequenceindex++){
+
+        //在该报数序列长度范围内遍历相邻的相同字符,当字符发生改变从头开始计数,此时countcountSequenceindex指向发生改变的字符
+        if(countSequenceindex==0||preCountSequence[countSequenceindex-1]==preCountSequence[countSequenceindex])
+            charCount+=1;
+        else{
+            //在报数序列末尾依次添上每组相同字符数和字符
+            s+=std::to_string(charCount)+preCountSequence[countSequenceindex-1];
+            charCount=1;
+        }
+        //与前一位相等时最后一组的charCount和tmp未插入字符串;
+        //与前一位不相等时当前位的charCount和tmp未插入字符串
+        if(countSequenceindex==preCountSequence.size()-1)
+            s+=std::to_string(charCount)+preCountSequence[countSequenceindex];
+    }
+    return s;
+}
+
+std::string countAndSayRecursive(int n){
+        if (n == 1) {
+            return "1";
+        }
+        return countAndSayLine(countAndSay(n - 1));
+}
+
 int main(){
-    std::cout<<countAndSay(15)<<std::endl;
+    std::cout<<countAndSayRecursive(4)<<std::endl;
 }
