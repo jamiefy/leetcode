5ca438b3a3de7a857c0176daa63a48f09c0da2c7
rannny <1369296693@qq.com>
2019-10-24 12:02:06 +0800

    'emplace_back不能使用列表初始化问题的解决'

--- foursum.cpp
+++ foursum.cpp
@@ -23,8 +23,13 @@ std::vector<std::vector<int>> fourSum(std::vector<int>& nums, int target) {
                 else if (nums[low] + nums[high] > twoSum)
                     high--;
                 else {
-                    //emplace_back调用形式更加简洁，直接根据参数初始化临时对象的成员，而push_back是压入对象，需要触发拷贝构造和转移构造
-                    //TODO:注意在此处使用ret.emplace_back(nums[curOne], nums[curTwo], nums[low], nums[high])会报错,为什么?
+                    //emplace_back调用形式更加简洁，直接根据参数初始化临时对象的成员，也就是只调用了一次构造函数
+                    // 而push_back是压入对象，需要触发拷贝构造和转移构造
+                    //注意在此处使用ret.emplace_back(nums[curOne], nums[curTwo], nums[low], nums[high])是错误的，
+                    //因为std::vector<std::vector<int>>里面vector类没有直接使用参数进行初始化的构造函数，必须使用列表
+                    //ret.emplace_back(std::vector<int>({nums[curOne], nums[curTwo], nums[low], nums[high]}));正确
+                    //ret.emplace_back({nums[curOne], nums[curTwo], nums[low], nums[high]});错误：emplace_back只能填
+                    //TODO:参数或类的对象不能填列表（即使vector有列表初始化）
                     ret.push_back({nums[curOne], nums[curTwo], nums[low], nums[high]});
                     while (low + 1 < high && nums[low + 1] == nums[low])
                         low++;
@@ -49,5 +54,4 @@ int main(){
     for(auto vector:ret){
         std::cout<<vector[0]<<" "<<vector[1]<<" "<<vector[2]<<" "<<vector[3]<<" "<<std::endl;
     }
-
 }
---
+++ solveSudoku.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Administrator on 2019/10/24.
+//
+#include <iostream>
+#include <vector>
+
+void solveSudoku(std::vector<std::vector<char>>& board) {
+    
+}
