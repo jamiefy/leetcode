b3e0480dd37d086d35e523b062ea57d01b20e03c
rannny <1369296693@qq.com>
2019-10-28 21:58:11 +0800

    '第43题字符串乘积易错思路'

---
+++ mutiply.cpp
@@ -0,0 +1,48 @@
+//
+// Created by 54293 on 2019/10/28.
+//
+#include <iostream>
+#include <vector>
+
+std::string multiply(std::string num1, std::string num2) {
+    std::vector<int> multiplyVec;
+    int len1=num1.size();
+    int len2=num2.size();
+    int  carry=0,index1=len1-1,index2=len2-1;
+    int n1=0,n2=0;
+    while(index1>=0&&index2>=0){
+        n1=num1[index1]-'0';
+        n2=num2[index2]-'0';
+        multiplyVec.emplace_back((n1*n2+carry)%10);
+        carry=(n1*n2+carry)/10;
+        index1--;
+        index2--;
+    }
+    if(index1<0){
+        while(index2>=0){
+            n2=num2[index2]-'0';
+            multiplyVec.emplace_back((n2+carry)%10);
+            carry=(n1+carry)/10;
+            index2--;
+        }
+    } else{
+        while(index1>=0){
+            n1=num1[index1]-'0';
+            multiplyVec.emplace_back((n1+carry)%10);
+            carry=(n1+carry)/10;
+            index1--;
+        }
+    }
+    if(carry!=0)
+        multiplyVec.emplace_back(carry);
+    std::string multiplyStr;
+    for(auto c:multiplyVec){
+        multiplyStr+=(char)(c+'0');
+    }
+    return multiplyStr;
+}
+
+int main(){
+    std::cout<<multiply("123","10")<<std::endl;
+
+}
