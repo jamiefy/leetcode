dce817a4ec0012f91fef6257e4d34ef7726c0e5b
jamiefy <1369296693@qq.com>
2019-12-25 10:11:48 +0800

    '第60题第k个排列minor'

--- getPermutation60.cpp
+++ getPermutation60.cpp
@@ -7,11 +7,8 @@
 //递归
 //时间复杂度：O(n)
 //空间复杂度：O(n)
-std::string getPermutation(int n, int k,std::vector<int>& visited,std::string& ret) {
-    //当字符串中数字个数等于总个数函数结束（注意不能等于n,因为n每次递归都在递减）
-    if(ret.size()==visited.size()-1){
-        return ret;
-    }
+std::string getPermutation(int n, int k,std::vector<int>& visited) {
+
     //如果使用5个数则可以将总排列分为5组，每组num=4*3*2个，计算k在哪组
     int num=1;
     for(int i=n-1;i>1;i--){
@@ -26,12 +23,11 @@ std::string getPermutation(int n, int k,std::vector<int>& visited,std::string& r
         if(!visited[position])
             i++;
     }
-    ret+=std::to_string(position);
     visited[position]=true;
     //k在第count组的第k（计算出来的k）个
     k=k%num==0 ? num : k%num;
     //循环递归
-    return getPermutation(n-1,k,visited,ret);
+    return n==1 ? std::to_string(position) : std::to_string(position)+getPermutation(n-1,k,visited);
 }
 std::string getPermutation(int n, int k) {
     //保证输入下面函数的n和k都大于1
@@ -40,7 +36,7 @@ std::string getPermutation(int n, int k) {
     std::string ret;
     //创建visited数组记录从1到n数字是否被使用过，下标对应操作更简单（0下标对应位不用）
     std::vector<int> visited(n+1, false);
-    return getPermutation(n,k,visited,ret);
+    return getPermutation(n,k,visited);
 }
 
 int main(){
