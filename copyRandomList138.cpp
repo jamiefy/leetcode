ada3c5f280506a7d7dc23adc801c7bb2620be3ed
anranzhai <1369296693@qq.com>
2020-02-29 19:14:23 +0800

    138复制复杂列表补充哈希表回溯

--- copyRandomList138.cpp
+++ copyRandomList138.cpp
@@ -2,6 +2,7 @@
 // Created by anranzhai on 2020/2/28.
 //
 #include <iostream>
+#include <map>
 class Node {
 public:
     int val;
@@ -45,14 +46,76 @@ Node* copyRandomList(Node* head) {
     return copyHead;
 }
 
-//哈希表时间复杂度O(n)空间复杂度O(n)
+//哈希表时间复杂度O(n)空间复杂度O(n),执行时间最多
 Node* copyRandomListHash(Node* head) {
-
+    if(head==NULL)return NULL;
+    std::map<Node*,Node*> nMap;
+    Node* curr=head;
+    while(curr!=NULL){
+        nMap[curr]=new Node(curr->val);
+        curr=curr->next;
+    }
+    Node* copyHead=nMap[head];
+    curr=head;
+    while(curr!=NULL){
+        if(curr->next!=NULL)
+            nMap[curr]->next=nMap[curr->next];
+        if(curr->random!= nullptr)
+            nMap[curr]->random=nMap[curr->random];
+         curr=curr->next;
+    }
+    return copyHead;
 }
 
+//哈希表优化,执行时间极大减少
+std::map<Node*,Node*> map;
+Node* getCloned(Node* node){
+    if(node==NULL)
+        return NULL;
+    if(map.find(node)==map.end())
+        map[node]=new Node(node->val);
+    return map[node];
+}
+Node* copyRandomLHash(Node* head) {
+    if(head==NULL)return NULL;
+    Node* copyHead=new Node(head->val),*copyCurr=copyHead,*curr=head;
+    map[head]=copyHead;
+    while(curr!=NULL){
+        copyCurr->next=getCloned(curr->next);
+        copyCurr->random=getCloned(curr->random);
+        curr=curr->next;
+        copyCurr=copyCurr->next;
+    }
+    return copyHead;
+}
+
+Node* copyNode(Node* node,std::map<Node*,Node*> &nodeMap) {
+    if(nodeMap[node]==NULL){
+        nodeMap[node]=new Node(node->val);
+        if(node->next!=NULL)
+            nodeMap[node]->next=copyNode(node->next,nodeMap);
+        if(node->random!=NULL)
+            nodeMap[node]->random=copyNode(node->random,nodeMap);
+    }
+    return nodeMap[node];
+}
 //回溯时间复杂度O(n)空间复杂度O(n)
 Node* copyRandomListRecursive(Node* head) {
+    std::map<Node*,Node*> nodeMap;
+    if(head==NULL)return NULL;
+    return copyNode(head,nodeMap);
+}
 
+//回溯,内存消耗最多
+std::map<Node*,Node*> nMap;
+Node* copyRandomListRecur(Node* head) {
+    if(head==NULL)return NULL;
+    if(nMap.find(head)!=nMap.end())
+        return nMap[head];
+    nMap[head]=new Node(head->val);
+    nMap[head]->next=copyRandomListRecur(head->next);
+    nMap[head]->random=copyRandomListRecur(head->random);
+    return nMap[head];
 }
 
 int main(){
@@ -64,7 +127,7 @@ int main(){
     head->random=second;
     second->random=head;
     third->random=second;
-    Node* copy=copyRandomList(head);
+    Node* copy=copyRandomLHash(head);
     while(copy!=NULL){
         std::cout<<copy->val<<"  "<<copy->random->val<<std::endl;
         copy=copy->next;
---
+++ search81.cpp
@@ -0,0 +1,13 @@
+//
+// Created by anranzhai on 2020/2/29.
+//
+#include <vector>
+bool search(std::vector<int>& nums, int target) {
+    if(nums.size()==0||nums[0]!=target)
+        return false;
+    int left=0,right=nums.size();
+    while(left<right){
+
+    }
+    return nums[left]==target;
+}
