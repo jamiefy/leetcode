61e319a7eb4b31a59f7593724ae01d3d22661189
anranzhai <1369296693@qq.com>
2020-03-23 09:27:50 +0800

    142环形链表相遇点to环入口=头结点to环入口

--- hasCycle141.cpp
+++ hasCycle141.cpp
@@ -10,13 +10,41 @@ struct ListNode {
 bool hasCycle(ListNode *head) {
     if(head==NULL)
         return false;
-    ListNode* ahead=head;
-    ListNode* behind=head->next;
-    while(behind!=NULL&&behind->next!=NULL){
+    ListNode* behind=head;
+    ListNode* ahead=head->next;
+    while(ahead!=NULL&&ahead->next!=NULL){
         if(ahead==behind)
             return true;
-        ahead=ahead->next;
-        behind=behind->next->next;
+        behind=behind->next;
+        ahead=ahead->next->next;
     }
     return false;
+}
+ListNode* hasCycleNode(ListNode *head) {
+    if(head==NULL)
+        return NULL;
+    ListNode* behind=head;
+    ListNode* ahead=head->next;
+    while(ahead!=NULL&&ahead->next!=NULL){
+        if(ahead==behind){
+            return ahead;
+        }
+        behind=behind->next;
+        ahead=ahead->next->next;
+    }
+    return NULL;
+}
+
+//给定阶段 1 找到的相遇点，阶段 2 将找到环的入口。首先我们初始化额外的两个指针： ptr1 ，指向链表的头， ptr2 指向相遇点。然后，我们每次将它们往前移动一步，直到它们相遇，它们相遇的点就是环的入口，返回这个节点。
+ListNode *detectCycle(ListNode *head) {
+    ListNode* behind=head;
+    ListNode* ahead=hasCycleNode(head);
+    if(ahead==NULL)
+        return NULL;
+    ahead=ahead->next;
+    while(behind!=ahead){
+        behind=behind->next;
+        ahead=ahead->next;
+    }
+    return behind;
 }
