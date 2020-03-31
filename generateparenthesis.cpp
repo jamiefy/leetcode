fd247809b31e91e8290ae8daf25528f0fec5d9fd
jamiefy <1369296693@qq.com>
2020-03-31 12:01:52 +0800

    22括号生成回溯闭合数

--- generateparenthesis.cpp
+++ generateparenthesis.cpp
@@ -5,19 +5,19 @@
 #include <vector>
 
 //闭合数k
+//为了枚举某些内容，我们通常希望将其表示为更容易计算的不相交子集的总和。考虑有效括号序列 S 的 闭包数：至少存在 index >= 0，
+//使得 S[0], S[1], ..., S[2*index+1]是有效的。 显然，每个括号序列都有一个唯一的闭包号。 我们可以尝试单独列举它们。
 std::vector<std::string> generateParenthesis(int n) {
-    std::vector<std::string> ret;
     if(n==0)
-        ret.emplace_back("");
-    else{
-        for(int k=0;k<n;k++){
-            for(auto left:generateParenthesis(k)){
-                for(auto right:generateParenthesis(n-1-k)){
-                    ret.emplace_back('('+left+')'+right);
-                }
+        return std::vector<std::string>{""};//切记不可用std::vector<std::string>();这是个空vector会在每一步
+        // 循环到auto left:generateParenthesis(0)的时候停止循环也不能执行下面往ret里添加元素的步骤
+    std::vector<std::string> ret;
+    for(int k=0;k<n;k++){
+        for(auto left:generateParenthesis(k)){
+            for(auto right:generateParenthesis(n-1-k)){
+                ret.emplace_back('('+left+')'+right);
             }
         }
-
     }
     return ret;
 }
@@ -73,3 +73,9 @@ std::vector<std::string> generateParentheisEnumerateing(int n) {
     return ret;
 }
 
+int main(){
+    std::vector<std::string> vec=generateParenthesis(3);;
+    for(auto v:vec){
+        std::cout<<v<<std::endl;
+    }
+}
