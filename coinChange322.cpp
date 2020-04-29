d4a23af7a80dd09dbf7a99a74e70275c661a8737
jamiefy <1369296693@qq.com>
2020-04-29 14:25:59 +0800

    岛屿bfs/dfs/并查集

--- coinChange322.cpp
+++ coinChange322.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+using namespace std;
 void coinChange(std::vector<int>& coins, int amount, int c_index, int count, int& ans)
 {
     if (amount == 0)
@@ -12,6 +13,7 @@ void coinChange(std::vector<int>& coins, int amount, int c_index, int count, int
         return;
     }
     if (c_index == coins.size()) return;
+    // k + count < ans剪枝必不可少，当当前数量大于现有最小数量的时候停止继续向下进行无意义的运算
     for (int k = amount / coins[c_index]; k >= 0 && k + count < ans; k--)
     {
         coinChange(coins, amount - k * coins[c_index], c_index + 1, count + k, ans);
@@ -27,7 +29,64 @@ int coinChange(std::vector<int>& coins, int amount)
     return ans == INT_MAX ? -1 : ans;
 }
 
+//递归，正确但超时
+int minc=INT32_MAX;
+void coinChanget(vector<int>& coins, int amount,int index,int precnt){
+    if(amount<=0){
+        minc=amount==0?std::min(precnt,minc):minc;
+        return;
+    }
+    if(index==coins.size()){
+        return;
+    }
+    //实现剪枝就不会超时了只需使判断条件为cnt>=0&&cnt+precnt<=min
+    for(int cnt=amount/coins[index];cnt>=0;cnt--){
+        coinChanget(coins,amount-coins[index]*cnt,index+1,precnt+cnt);
+    }
+}
+int coinChanget(vector<int>& coins, int amount) {
+    if(coins.size()==0)
+        return -1;
+    sort(coins.begin(),coins.end(),greater<int>());
+    int coin=coins[0];
+    for(int cnt=amount/coin;cnt>=0;cnt--){
+        coinChanget(coins,amount-coin*cnt,1,cnt);
+    }
+    return minc==INT32_MAX?-1:minc;
+}
+
+//回溯，不正确，当14=10 1 1 1 1就直接停止运算了但是最优解是14=7 7
+int mincnt=INT32_MAX;
+bool coinChangetest(vector<int>& coins, int amount,int index,int precnt){
+    if(amount==0){
+        mincnt=std::min(precnt,mincnt);
+        return true;
+    }
+    if(amount<0)
+        return false;
+    if(index==coins.size()){
+        return true;
+    }
+    for(int cnt=amount/coins[index];cnt>=0;cnt--){
+        if(coinChangetest(coins,amount-coins[index]*cnt,index+1,precnt+cnt))
+            return true;
+    }
+    return false;
+}
+int coinChangetest(vector<int>& coins, int amount) {
+    if(coins.size()==0)
+        return -1;
+    sort(coins.begin(),coins.end(),greater<int>());
+    int coin=coins[0];
+    for(int cnt=amount/coin;cnt>=0;cnt--){
+        if(coinChangetest(coins,amount-coin*cnt,1,cnt))
+            break;
+    }
+    return mincnt==INT32_MAX?-1:mincnt;
+}
+
+
 int main(){
-    std::vector<int> vec{10,1,7};
-    std::cout<<coinChange(vec,14);
+    std::vector<int> vec{10,3,7};
+    std::cout<<coinChange(vec,39);
 }
--- numIsLand200.cpp
+++ numIsLand200.cpp
@@ -1,4 +1,146 @@
 //
 // Created by anranzhai on 2020/4/28.
 //
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
 
+//广度优先搜索
+//时间复杂度：O(MN)，其中 MM 和 NN 分别为行数和列数。
+//
+//空间复杂度：O(\min(M, N))，在最坏情况下，整个网格均为陆地，队列的大小可以达到min(M,N)。
+int rowdir[4]={-1,0,+1,0};
+int coldir[4]={0,+1,0,-1};
+void bfs(vector<vector<char>>& grid,int row,int col) {
+    queue<pair<int, int>> land;
+    land.emplace(row, col);
+    //在压入队列的时候就更改grid对应位置的值表示该位置访问过，避免重复压入队列,而不是在弹出的时候更改状态
+    grid[row][col] = '0';
+    while (!land.empty()) {
+        int i = land.front().first;
+        int j = land.front().second;
+        land.pop();
+        for (int k = 0; k < 4; k++) {
+            int x = i + rowdir[k];
+            int y = j + coldir[k];
+            if (x < 0 || y < 0 || x > grid.size() - 1|| y > grid[0].size() - 1 ||grid[x][y] == '0')
+                continue;
+            else{
+                land.emplace(x, y);
+                grid[x][y]='0';
+            }
+        }
+    }
+}
+
+//深度优先搜索
+//时间复杂度：O(MN)，其中 M 和 N 分别为行数和列数。
+//空间复杂度：O(MN)，在最坏情况下，整个网格均为陆地，深度优先搜索的深度达到 MN。
+void dfs(vector<vector<char>>& grid,int row,int col){
+    grid[row][col]='0';
+    if(row-1>=0&&grid[row-1][col]=='1')
+        dfs(grid,row-1,col);
+    if(col+1<grid[0].size()&&grid[row][col+1]=='1')
+        dfs(grid,row,col+1);
+    if(row+1<grid.size()&&grid[row+1][col]=='1')
+        dfs(grid,row+1,col);
+    if(col-1>=0&&grid[row][col-1]=='1')
+        dfs(grid,row,col-1);
+}
+
+int numIslands(vector<vector<char>>& grid) {
+    if(grid.size()==0)
+        return 0;
+    int ans=0;
+    for(int i=0;i<grid.size();i++){
+        for(int j=0;j<grid[0].size();j++){
+            if(grid[i][j]=='1'){
+                dfs(grid,i,j);
+                ans++;
+            }
+        }
+    }
+    return ans;
+}
+//并查集
+class UnionFind {
+public:
+    UnionFind(vector<vector<char>>& grid) {
+        count = 0;
+        int m = grid.size();
+        int n = grid[0].size();
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == '1') {
+                    parent.push_back(i * n + j);
+                    ++count;
+                }
+                else {
+                    parent.push_back(-1);
+                }
+                rank.push_back(0);
+            }
+        }
+    }
+
+    int find(int i) {
+        //并查集的压缩路径，使每个节点都指向根节点
+        return parent[i]==i?i:(parent[i]=find(parent[i]));
+    }
+
+    void unite(int x, int y) {
+        int rootx = find(x);
+        int rooty = find(y);
+        if (rootx != rooty) {
+            if (rank[rootx] < rank[rooty])
+                parent[rootx]=rooty;
+            else{
+                parent[rooty]=rootx;
+                if (rank[rootx] == rank[rooty]) rank[rootx] += 1;
+            }
+            --count;
+        }
+    }
+
+    int getCount() const {
+        return count;
+    }
+
+private:
+    vector<int> parent;
+    vector<int> rank;
+    int count;
+};
+
+int numLand(vector<vector<char>> &grid) {
+    int nr = grid.size();
+    if (!nr) return 0;
+    int nc = grid[0].size();
+
+    UnionFind uf(grid);
+    int num_islands = 0;
+    for (int r = 0; r < nr; ++r) {
+        for (int c = 0; c < nc; ++c) {
+            if (grid[r][c] == '1') {
+                grid[r][c] = '0';
+                //对相邻的节点进行相连
+                if (r - 1 >= 0 && grid[r-1][c] == '1') uf.unite(r * nc + c, (r-1) * nc + c);
+                if (r + 1 < nr && grid[r+1][c] == '1') uf.unite(r * nc + c, (r+1) * nc + c);
+                if (c - 1 >= 0 && grid[r][c-1] == '1') uf.unite(r * nc + c, r * nc + c - 1);
+                if (c + 1 < nc && grid[r][c+1] == '1') uf.unite(r * nc + c, r * nc + c + 1);
+            }
+        }
+    }
+
+    return uf.getCount();
+}
+
+
+int main(){
+    vector<vector<char>> vec{{'1','1','0','0','0'},
+                             {'1','1','0','0','0'},
+                             {'0','0','1','0','0'},
+                             {'0','0','0','1','1'}};
+    cout<<numIslands(vec);
+}
