65cf87b9d9b0a8da7553d3373a9af41e1118556f
jamiefy <1369296693@qq.com>
2020-05-03 22:28:03 +0800

    94中序遍历morris保持树原有结构

--- inorderTraversal94.cpp
+++ inorderTraversal94.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <vector>
 #include <stack>
+using namespace std;
 struct TreeNode {
     int val;
     TreeNode *left;
@@ -23,13 +24,11 @@ void inorderTra(TreeNode* root,std::vector<int> &vec){
 //空间复杂度：最坏情况下需要空间O(n)，平均情况为O(logn)。
 std::vector<int> inorderTraversal(TreeNode* root) {
     std::vector<int> ret;
-    if(root==NULL)
-        return ret;
     inorderTra(root,ret);
     return ret;
 }
 
-//栈,时间复杂度空间复杂度均为O(n)
+//栈,时间复杂度空间复杂度均为O(n),运行时间快，消耗空间少
 std::vector<int> inorderTraverse(TreeNode* root) {
     std::vector<int> ret;
     if(root==NULL)
@@ -42,13 +41,14 @@ std::vector<int> inorderTraverse(TreeNode* root) {
             curr=curr->left;
         }
         curr=tmp.top();
+        tmp.pop();
         ret.push_back(curr->val);
         curr=curr->right;
     }
     return ret;
 }
 
-//线索二叉树,时间复杂度空间复杂度均为O(n)
+//线索二叉树,时间复杂度空间复杂度均为O(n),改变树结构
 std::vector<int> inorderTraver(TreeNode* root) {
     std::vector<int> ret;
     if(root==NULL)
@@ -67,6 +67,60 @@ std::vector<int> inorderTraver(TreeNode* root) {
             curr->left=NULL;
             pre->right=curr;
             curr=tmp;
+        }
+    }
+    return ret;
+}
+
+
+vector<int> inorderTraversalagain(TreeNode* root) {
+    if(root==NULL)
+        return vector<int>();
+    vector<int> ret;
+    stack<TreeNode*> st;
+    st.emplace(root);
+    TreeNode* parent=root->left;
+    while(parent!=NULL||!st.empty()){
+        while(parent!=NULL){
+            st.emplace(parent);
+            parent=parent->left;
+        }
+        ret.emplace_back(st.top()->val);//中序遍历和前序遍历仅此一句位置不同
+        parent=st.top()->right;
+        st.pop();
+    }
+    return ret;
+}
+
+//morris（保持树原有结构）,空间消耗最少
+vector<int> inorderTraversalmirror(TreeNode* root) {
+    if(root==NULL)
+        return vector<int>();
+    vector<int> ret;
+    TreeNode* cur = root;
+    TreeNode* pre = NULL;
+    while(cur!=NULL) {
+        if (cur->left == NULL) {
+            ret.emplace_back(cur->val);
+            cur = cur->right;
+        } else {
+            pre = cur->left;
+            //找到前驱节点，即左子树中的最右节点
+            //pre->right!=NULL第一遍遍历到该最右节点
+            //pre->right!=cur第二遍遍历到该最右节点
+            while (pre->right != NULL && pre->right != cur)
+                pre = pre->right;
+            //创建中序遍历线索
+            if (pre->right == NULL) {
+                pre->right = cur;
+                cur = cur->left;
+            }
+            //恢复树原有结构
+            if (pre->right == cur) {
+                pre->right = NULL;
+                ret.emplace_back(cur->val);
+                cur = cur->right;
+            }
         }
     }
     return ret;
