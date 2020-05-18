ebf86968dac094752c2d34c314612e43986337ba
jamiefy <1369296693@qq.com>
2020-05-18 18:24:21 +0800

    The details are the devil

--- searchRange.cpp
+++ searchRange.cpp
@@ -3,7 +3,7 @@
 //
 #include <iostream>
 #include <vector>
-
+using namespace std;
 std::vector<int> searchRange(std::vector<int>& nums, int target) {
     std::vector<int> ret{-1,-1};
     int low=0,high=nums.size()-1;
@@ -106,7 +106,30 @@ int binarySearchBound(std::vector<int>& nums, int target) {
 
 }
 
+vector<int> searchRangenew(vector<int>& nums, int target) {
+    if(nums.size()==0)
+        return vector<int>{-1,-1};
+    int left=0;
+    int right=nums.size()-1;
+    while(left<=right){
+        int a=(right-left)>>1;
+        int mid=(right-left)/2+left;
+        if(nums[mid]==target){
+            int l=mid-1;
+            int r=mid+1;
+            while(nums[l]==target)l--;
+            while(nums[r]==target)r++;
+            return vector<int>{l+1,r-1};
+        }
+        if(nums[mid]<target)
+            left=mid+1;
+        else if(nums[mid]>target)
+            right=mid-1;
+    }
+    return vector<int>{-1,-1};
+}
+
 int main(){
-    std::vector<int> nums{1,2,3,4,5,6};
-    std::cout<<binarySearch(nums,7)<<" "<<binarySearchBound(nums,7)<<std::endl;
+    std::vector<int> nums{5,7,7,8,8,10};
+    std::cout<<searchRangenew(nums,8)[0]<<" "<<searchRangenew(nums,8)[1]<<std::endl;
 }
